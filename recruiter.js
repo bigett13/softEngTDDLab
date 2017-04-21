9bd8ecc9aa88d9acb4dfbd0a804223137fb1e491
Bigelow <bigett13@deacnet.wfu.edu>
2017-04-21 17:49:01 -0400

    final

--- recruiter.js
+++ recruiter.js
@@ -8,48 +8,88 @@ var util = require('./util.js');
 //TODO: You need to write this function AND utilize it.
 // bracketFromGPA(decimal GPA);
 function bracketFromGPA(gpa) {
+	if(gpa < 2.5)
+		return 0;
+	if(gpa < 3.0)
+		return 1;
+	if(gpa < 3.5)
+		return 2;
 	// 4-3.5, 3.49 - 3.0, 2.99 - 2.5
-	return; //some form of bracket number
+	return 3; //some form of bracket number
 }
 
 // TODO: recruiter( Array of hireables )
 function recruiter(internArr) {
 
+	var astrologyArray = internArr.slice();
+	astrologyArray = [];
+	var astrologyIndex = 0;
 	// Below is just to help show the syntax you need,
 	// you'll need to process ALL of the hireables like this one and sort
-	var index = 0;
-	var iname = internArr[index].name;
-	var idegr = internArr[index].degree;
-	var igpa = internArr[index].gpa;
-	var iexp = internArr[index].experiance;
-	var iwage, ivalue, ibracket, imetric;
+	for(var i = internArr.length - 1; i >= 0; i--){
+		var index = i;
+		var iname = internArr[index].name;
+		var idegr = internArr[index].degree;
+		var igpa = internArr[index].gpa;
+		var iexp = internArr[index].experiance;
+		var iwage, ivalue, ibracket, imetric;
 
-	// Yep, you can use strings as an "index" (technically it's a property) in JavaScript
-	idegr = idegr.toLowerCase();
-	iwage = degreeSWage[idegr];
+		idegr = idegr.toLowerCase();
+		iwage = degreeSWage[idegr];
 
-	// You should use these functions at some point
-	ivalue = util.getValueFromWageAndExp( /*wage, full years of experiance*/ );
-	ibracket = bracketFromGPA ( /*decimal GPA*/ );
+		if(idegr == "astrology"){ //for astrology degrees
+			astrologyArray[astrologyIndex] = internArr[index];
 
-	// Hmm... this doesn't seem to follow the spec - fix it
-	imetric = ivalue + ibracket;
+			internArr.splice(index, 1);
 
-	// We really want to add our sorting number "metric" to objects (it really is this easy)
-	internArr[index].metric = imetric;
+			ivalue = util.getValueFromWageAndExp(iwage, Math.floor(iexp));
+			ibracket = bracketFromGPA (igpa);
+
+			imetric = 4*ivalue + ibracket;
+			astrologyArray[astrologyIndex].metric = imetric;
+
+			astrologyIndex++;
+		}
+		else if(igpa < 2.5 || iwage == undefined){ //remove students under 2.5 gpa or unrecognized degree
+
+			internArr.splice(index, 1);
+
+			index = index - 1;
+		}
+		else{
+			ivalue = util.getValueFromWageAndExp(iwage, Math.floor(iexp));
+			ibracket = bracketFromGPA (igpa);
+
+			// Hmm... this doesn't seem to follow the spec - fix it
+			imetric = 4*ivalue + ibracket;
+
+			// We really want to add our sorting number "metric" to objects (it really is this easy)
+			internArr[index].metric = imetric;
+		}
+
+	}
 
 	// and then sort them all (it doesn't return anything, it modifies the array sent)
-	util.sortInternObjects( /*Array of hireables with "metric" as a property*/ );
+	util.sortInternObjects( internArr );
+	util.sortInternObjects( astrologyArray );	//sort astrology by metric
 
+	internArr = internArr.concat(astrologyArray);	//stick astrology at end of array
 
-	// Output 
+	// Output
 	// An array of HIREABLE 'intern objects' (in order of most valueable to least valueable)
 	// with at least the properties "name", "metric", "degree"
 	// You can come up with any number you want for "metric" as long as it corresponds to the spec
 	// and people earlier in the array have equal or greater values for "metric" than
 	// people further down.
 
-	return internArr;
+	if(internArr.length > 0){
+		for(var j = 0; j < internArr.length; j++){
+			console.log(internArr[j].name + " " + internArr[j].metric + " " + internArr[j].degree);
+		}
+		return internArr;
+	}else{
+		return astrologyArray;
+	}
 };
 
 module.exports = {
--- test/index.js
+++ test/index.js
@@ -17,7 +17,7 @@ test('util.getValueFromWageAndExp', function(t) {
   	t.fail('does not factor in experiance');
   }
 
-  t.equal(util.getValueFromWageAndExp(34, 1.3), false, 
+  t.equal(util.getValueFromWageAndExp(34, 1.3), false,
   	"getValueFromWageAndExp catches a partial year input and returns false");
 
   t.end();
@@ -31,7 +31,7 @@ test('util.sortInternObjects', function(t) {
 	inputArr[3].metric = 0;
 
 	// Lets get the input sorted manually, in the expected array
-	var expectedArr = inputArr.slice();
+	var expectedArr = JSON.parse(JSON.stringify(inputArr));
 	expectedArr = [
 		expectedArr[0], // 3
 		expectedArr[2], // 2
@@ -39,8 +39,8 @@ test('util.sortInternObjects', function(t) {
 		expectedArr[3]  // 0
 	];
 
-	// Lets make a copy of the input to sort with the function 
-	var actualArr = inputArr.slice();
+	// Lets make a copy of the input to sort with the function
+	var actualArr = JSON.parse(JSON.stringify(inputArr));
 
 	// Sort by reference (in-place)
 	util.sortInternObjects(actualArr);
@@ -66,6 +66,91 @@ test('util.sortInternObjects', function(t) {
 });
 
 // Your tests go here  (methods reference: https://www.npmjs.com/package/tape#testname-opts-cb )
+
+test('bracketFromGPA', function(t){
+  t.deepEqual( recruiter.bracketFromGPA(3.5), 3, "returns bracket three");
+  t.deepEqual( recruiter.bracketFromGPA(3.4), 2, "returns bracket two");
+  t.deepEqual( recruiter.bracketFromGPA(2.99), 1, "returns bracket one");
+  t.deepEqual( recruiter.bracketFromGPA(2.49), 0, "returns bracket zero (unhirable)");
+
+  t.end();
+});
+
+test('recruiter function', function(t){
+
+  t.comment("Don't hire people we do not recognize");
+  var collArr = [
+    interns[0],
+    interns[6],
+    interns[7]
+  ];
+
+  var inputArr = JSON.parse(JSON.stringify(collArr));
+  inputArr[1].degree = "waffle maker";
+  inputArr[2].degree = "";
+
+  var retArr = [];
+  retArr = recruiter.recruiter(inputArr);
+  t.deepEqual(retArr.length, 1, "Returns expected number of interns");
+  t.deepEqual(retArr[0].degree, "advertising", "returns the accepted degree");
+
+
+  //test astrology is last
+  t.comment("Test for astrology");
+
+  collArr = [
+    interns[5],
+    interns[14],
+    interns[0],
+    interns[1],
+    interns[2],
+  ]
+
+  inputArr = JSON.parse(JSON.stringify(collArr));
+
+  inputArr[1].degree = "astrology";
+  inputArr[0].degree = "astrology";
+  inputArr[1].gpa = 3.4;
+
+  t.ok(inputArr[0].gpa === 2.14,
+    inputArr[1].gpa == 3.4, "test input is as expected");
+
+  retArr = recruiter.recruiter(inputArr);
+  t.deepEqual(retArr.length, 5, "Returns expected number of interns");
+  t.ok(retArr[3].metric > retArr[4].metric, "astrology metric is correct");
+  t.deepEqual(retArr[4].degree, "astrology", "returns astrology degrees at end of array");
+
+
+  //sort secondarily by GPA Bracket
+  t.comment("Sort secondarily by GPA Bracket");
+
+  var collArr2 = [
+    interns[13],
+    interns[14],
+    interns[15],
+    interns[16],
+  ];
+
+  var inputArr2 = JSON.parse(JSON.stringify(collArr2));
+
+  inputArr2[0].experiance = 4;
+  inputArr2[0].degree = "human resources management";
+  inputArr2[3].experiance  = 0;
+  inputArr2[3].degree = "human resources management";
+
+  t.ok(inputArr2[0].gpa === 3.1 &&
+    inputArr2[1].gpa === 2.07 &&
+    inputArr2[2].gpa === 2.32 &&
+    inputArr2[3].gpa === 3.93, "test input is as expected");
+
+  var retArr2 = recruiter.recruiter(inputArr2);
+
+  t.deepEqual(retArr2.length, 2, "Returns expected number of interns, removes GPAs below 2.5");
+  t.deepEqual(retArr2[0].gpa, 3.1, "Returns expected GPA order");
+  t.ok(retArr2[0].metric > retArr2[1].metric, "Returns metrics in order");
+
+  t.end()
+});
 
 // test('Test Name', function(t) {
 
